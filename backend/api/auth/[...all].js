e3a819ebd0bbe2b17ec1d46314cae4fecd0c6f48
GioDP18 <giolagariza@gmail.com>
2026-07-16 12:26:08 +0800

    Add authentication middleware to handle database connection and seeding

---
+++ backend/api/auth/[...all].js
@@ -0,0 +1,19 @@
+const app = require('../../../src/app');
+const { connectDatabase } = require('../../../src/config/database');
+const { bootstrapSeed } = require('../../../src/utils/bootstrapSeed');
+
+let connectPromise = null;
+let seedPromise = null;
+
+module.exports = async (req, res) => {
+  if (!connectPromise) {
+    connectPromise = connectDatabase();
+  }
+
+  await connectPromise;
+  if (!seedPromise) {
+    seedPromise = bootstrapSeed();
+  }
+  await seedPromise;
+  return app(req, res);
+};
